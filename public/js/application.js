41c2a0815b5d625ccf8a53c1375fdf2254f57ec2
huayun321 <huayun321@gmail.com>
2016-02-19 15:45:04 +0800

    更新share

--- public/js/application.js
+++ public/js/application.js
@@ -2,9 +2,9 @@
 window.requestAnimationFrame(function () {
 
   var winScore = {
-    's8' : 'p83Xcs0a56gO1RtcAthkn9wC68tg',
-    's128' : 'p83Xcs0a56gO1RtcAthkn9wC68tg',
-    's256' : 'p83Xcs5WNCIJxXiA7DuWEi69Pcbg',
+    's8'   : 'p83Xcs0a56gO1RtcAthkn9wC68tg',
+    's16' : 'p83Xcs0a56gO1RtcAthkn9wC68tg',
+    's32' : 'p83Xcs5WNCIJxXiA7DuWEi69Pcbg',
     's1024': 'p83Xcs0TuD-sZebI1cgOQSzhtS_Q',
     's2048': 'p83Xcs9Qv86hPvVsCdr0nIc52r_Y'
   }
