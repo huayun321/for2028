86859617eba7f3e23f011e3287220e9ffa942571
huayun321 <huayun321@gmail.com>
2016-02-25 10:30:06 +0800

    finish pingpong card matching

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var pingpong = require('./routes/pingpong');
+var cardmatching = require('./routes/cardmatching');
+
 
 
 var app = express();
@@ -27,6 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/pingpong', pingpong);
+app.use('/cardmatching', cardmatching);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
